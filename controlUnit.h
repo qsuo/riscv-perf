a9985cf79c49be17a1a97f6dac8bf6f14e65618d
U-CCR\dyarovoy <dyarovoy@dyarovoy-MOBL.ccr.corp.intel.com>
2019-12-08 22:37:05 +0300

    add control unit

---
+++ controlUnit.h
@@ -0,0 +1,56 @@
+
+#ifndef CONTROL_UNIT_H
+#define CONTROL_UNIT_H
+
+#include <unordered_map>
+
+#include "controlUnit.h"
+
+enum Opcode
+{
+    OP_IMM      = 0b0010011,
+    OP          = 0b0110011,
+    LUI         = 0b0110111,
+    AUIPC       = 0b0010111,
+    JAL         = 0b1101111,
+    JALR        = 0b1100111,
+    BRANCH      = 0b1100011,
+    LOAD        = 0b0000011,
+    STORE       = 0b0100011,
+    MISC_MEM    = 0b0001111,
+    SYSTEM      = 0b1110011
+};
+
+enum AluOp
+{
+    ADD,
+    SUB
+};
+
+class ControlUnit
+{
+public:
+
+    struct Input
+    {
+        uint32_t encoding;
+    };
+
+    struct Output
+    {
+        int aluOp;
+        int aluSrc2;
+        int brnCond;
+        int memWe;
+        int we;
+        int wbCtrl;
+    };
+
+    using unordered_map<uint32_t, Output> = map;
+
+
+
+    Output operate(Input& t);
+};
+
+#endif
--- mux.h
+++ mux.h
@@ -2,12 +2,32 @@
 #ifndef MUX_H
 #define MUX_H
 
-template <int size>
+template <int Tsize>
 class Mux
 {
 public:
 
-    uint32_t input[size];
+    struct Input
+    {
+        int a;
+        uint32_t in1;
+        uint32_t in2;
+    };
+
+    struct Output
+    {
+        uint32_t d;
+    };
+
+    Output operate(Input& input)
+    {
+        Output output;
+        if(input.a == 0)
+            output.d = input.in1;
+        else
+            output.d = input.in2;
+        return output;
+    }
 
 };
 

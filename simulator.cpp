e2b174c78222f5e2557938e5683a16348723b7a4
Ilya Podgorny <ilya.podgorny@intel.com>
2019-12-09 16:51:35 +0300

    simulator: add execute initial draft

--- simulator.cpp
+++ simulator.cpp
@@ -6,11 +6,41 @@ Simulator::Simulator(uint32_t memory_size)
     i_mem.attachMemory(&memory);
 }
 
-
 void Simulator::doFetch() {}
 void Simulator::doDecode() {}
-void Simulator::doExecute() {}
-void Simulator::doMemory() {
+
+void Simulator::doExecute()
+{
+    DecodeReg in = decode_reg.get();
+    uint32_t src1 = in.d1;
+    uint32_t src2 = in.d2; // src for alu and wd for memory
+    uint32_t alu_res;
+
+    if (in.flags.aluSrc2 == 1)
+        src2 = in.imm;   // TODO: process immediate
+
+    switch (in.flags.aluOp)
+    {
+        case AluOp::ADD:
+            alu_res = src1 + src2;
+            break;
+        case AluOp::SUB:
+            alu_res = src1 - src2;
+            break;
+        default:
+            break;
+    }
+
+
+    ExecuteReg out = {.pc = in.pc, .flags = in.flags, .alu_res = alu_res,
+                      .wd = src2, .wb_a = in.wb_a};
+    execute_reg.set(out);
+    // TODO: branches, signed/unsigned comparisons
+
+}
+
+void Simulator::doMemory()
+{
     ExecuteReg in = execute_reg.get();
     uint32_t wb_d = in.alu_res;
     // alu_res is address and wb_data if no memory operation

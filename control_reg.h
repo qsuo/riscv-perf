ebdd8f01cc11316c8a79350e4abc04570db42c0c
Ilya Podgorny <ilya.podgorny@intel.com>
2019-12-09 15:18:45 +0300

    add pc to control regs

--- control_reg.h
+++ control_reg.h
@@ -5,49 +5,60 @@
 #ifndef CONTROL_REG_H
 #define CONTROL_REG_H
 
+using address_t = uint32_t;
 using flags_t = ControlUnit::Output;
 
 template <class RegType>
 class ControlReg
 {
 public:
+    ControlReg() {}
     ControlReg(RegType& data) : data_(data) {}
 
-    RegType getData() const { return data_; }
-    setData(RegType& data) { data_ = data; };
+    RegType get() const { return data_; }
+    void set(RegType& data) { data_ = data; };
 
 private:
     RegType data_;
 };
 
+struct PCReg
+{
+    address_t pc;
+};
+
 struct FetchReg
 {
+    address_t pc;
     uint32_t encoding;
 };
 
 struct DecodeReg
 {
+    address_t pc;
     flags_t flags;
     uint8_t wb_a;   // write back address
     uint32_t d1;    // data
     uint32_t d2;    // data
     bool se;        // sign extension
-    uint32_t imm; // type ?
+    uint32_t imm;   // type ?
 };
 
 struct ExecuteReg
 {
+    address_t pc;
     flags_t flags;
     uint32_t alu_res;
     uint32_t wd;    // write data (for memory)
-    uint8_t wb_a;
+    uint8_t wb_a;   // write back address
 };
 
 struct MemoryReg
 {
+    address_t pc;
     flags_t flags;
-    uint8_t wb_a;
-    uint32_t wb_d;  // write back address
+    uint8_t wb_a;   // write back address
+    uint32_t wb_d;  // write back data (alu_res or result of memory read)
 };
 
 #endif
